5eb9a555e0bdf2839afb5f4e7c3e9cc459b6d40e
RealMbrads <65929196+RealMbrads@users.noreply.github.com>
2025-06-26 18:17:25 -0700

    Add files via upload

---
+++ IP Indentifer V4/ConsoleApp1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace IP_Identifier_WindowsForms
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnLookup;
+        private System.Windows.Forms.RichTextBox rtbResults;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.LinkLabel linkLabelMaps;
+        private System.Windows.Forms.Button btnMyIP;
+        private System.Windows.Forms.Label label2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnLookup = new System.Windows.Forms.Button();
+            this.rtbResults = new System.Windows.Forms.RichTextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.linkLabelMaps = new System.Windows.Forms.LinkLabel();
+            this.btnMyIP = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(15, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(260, 22);
+            this.txtIP.TabIndex = 0;
+            // 
+            // btnLookup
+            // 
+            this.btnLookup.Location = new System.Drawing.Point(285, 10);
+            this.btnLookup.Name = "btnLookup";
+            this.btnLookup.Size = new System.Drawing.Size(80, 26);
+            this.btnLookup.TabIndex = 1;
+            this.btnLookup.Text = "Lookup";
+            this.btnLookup.UseVisualStyleBackColor = true;
+            this.btnLookup.Click += new System.EventHandler(this.btnLookup_Click);
+            // 
+            // rtbResults
+            // 
+            this.rtbResults.Location = new System.Drawing.Point(15, 45);
+            this.rtbResults.Name = "rtbResults";
+            this.rtbResults.ReadOnly = true;
+            this.rtbResults.Size = new System.Drawing.Size(350, 180);
+            this.rtbResults.TabIndex = 2;
+            this.rtbResults.Text = "";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(285, 260);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(80, 28);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // linkLabelMaps
+            // 
+            this.linkLabelMaps.ActiveLinkColor = System.Drawing.Color.Blue;
+            this.linkLabelMaps.AutoSize = true;
+            this.linkLabelMaps.Location = new System.Drawing.Point(15, 230);
+            this.linkLabelMaps.Name = "linkLabelMaps";
+            this.linkLabelMaps.Size = new System.Drawing.Size(0, 16);
+            this.linkLabelMaps.TabIndex = 6;
+            this.linkLabelMaps.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelMaps_LinkClicked);
+            // 
+            // btnMyIP
+            // 
+            this.btnMyIP.Location = new System.Drawing.Point(15, 260);
+            this.btnMyIP.Name = "btnMyIP";
+            this.btnMyIP.Size = new System.Drawing.Size(80, 28);
+            this.btnMyIP.TabIndex = 4;
+            this.btnMyIP.Text = "My IP";
+            this.btnMyIP.UseVisualStyleBackColor = true;
+            this.btnMyIP.Click += new System.EventHandler(this.btnMyIP_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(110, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 42);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Public IP Addresses ONLY";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // Removed label2.Click event hookup
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(380, 300);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnMyIP);
+            this.Controls.Add(this.linkLabelMaps);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.rtbResults);
+            this.Controls.Add(this.btnLookup);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "IP Identifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ IP Indentifer V4/ConsoleApp1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace IP_Identifier_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        private const string ipInfoToken = "";     // Put your ipinfo.io token here
+        private const string vpnApiKey = "";         // Put your vpnapi.io key here
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.txtIP.KeyPress += new KeyPressEventHandler(txtIP_KeyPress);
+        }
+
+        private async void btnLookup_Click(object sender, EventArgs e)
+        {
+            string ip = txtIP.Text.Trim();
+            if (string.IsNullOrEmpty(ip))
+            {
+                MessageBox.Show("Please enter an IP address.");
+                return;
+            }
+
+            try
+            {
+                // Step 1: Get general info from ipinfo.io
+                string ipInfoUrl = $"https://ipinfo.io/{ip}/json?token={ipInfoToken}";
+                IpInfoResponse ipInfoData;
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage ipInfoResponse = await client.GetAsync(ipInfoUrl);
+                    ipInfoResponse.EnsureSuccessStatusCode();
+                    string ipInfoJson = await ipInfoResponse.Content.ReadAsStringAsync();
+                    ipInfoData = JsonConvert.DeserializeObject<IpInfoResponse>(ipInfoJson);
+                }
+
+                // Step 2: Get VPN info from vpnapi.io
+                string vpnApiUrl = $"https://vpnapi.io/api/{ip}?key={vpnApiKey}";
+                VpnApiResponse vpnData;
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage vpnResponse = await client.GetAsync(vpnApiUrl);
+                    vpnResponse.EnsureSuccessStatusCode();
+                    string vpnJson = await vpnResponse.Content.ReadAsStringAsync();
+                    vpnData = JsonConvert.DeserializeObject<VpnApiResponse>(vpnJson);
+                }
+
+                // Build combined results string
+                string results = $"IP: {ipInfoData.ip}\n" +
+                                 $"Country: {ipInfoData.country}\n" +
+                                 $"Region: {ipInfoData.region}\n" +
+                                 $"City: {ipInfoData.city}\n" +
+                                 $"Postal Code: {ipInfoData.postal}\n" +
+                                 $"Coordinates: {ipInfoData.loc}\n" +
+                                 $"ISP: {ipInfoData.org}\n" +
+                                 $"Timezone: {ipInfoData.timezone}\n" +
+                                 $"VPN: {(vpnData.security.vpn ? "Yes" : "No")}\n" +
+                                 $"Proxy: {(vpnData.security.proxy ? "Yes" : "No")}\n" +
+                                 $"TOR: {(vpnData.security.tor ? "Yes" : "No")}\n";
+
+                rtbResults.Text = results;
+
+                // Update map link using ipinfo.io coords
+                if (!string.IsNullOrEmpty(ipInfoData.loc))
+                {
+                    string[] coords = ipInfoData.loc.Split(',');
+                    if (coords.Length == 2)
+                    {
+                        linkLabelMaps.Text = $"Open Map: {coords[0]}, {coords[1]}";
+                        linkLabelMaps.Tag = $"https://www.google.com/maps/?q={coords[0]},{coords[1]}";
+                        linkLabelMaps.LinkColor = System.Drawing.Color.Blue;
+                    }
+                    else
+                    {
+                        linkLabelMaps.Text = "";
+                        linkLabelMaps.Tag = null;
+                    }
+                }
+                else
+                {
+                    linkLabelMaps.Text = "";
+                    linkLabelMaps.Tag = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async void btnMyIP_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string myIP = await client.GetStringAsync("https://api.ipify.org");
+                    txtIP.Text = myIP;
+                    btnLookup.PerformClick();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to get your IP: " + ex.Message);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void linkLabelMaps_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (linkLabelMaps.Tag != null)
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = linkLabelMaps.Tag.ToString(),
+                    UseShellExecute = true
+                });
+            }
+        }
+
+        private void txtIP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                btnLookup.PerformClick();
+            }
+        }
+    }
+
+    // ipinfo.io response model
+    public class IpInfoResponse
+    {
+        public string ip { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public string loc { get; set; }
+        public string org { get; set; }
+        public string postal { get; set; }
+        public string timezone { get; set; }
+    }
+
+    // vpnapi.io response model
+    public class VpnApiResponse
+    {
+        public string ip { get; set; }
+        public Location location { get; set; }
+        public Security security { get; set; }
+    }
+
+    public class Location
+    {
+        public string country { get; set; }
+        public string region { get; set; }
+        public string city { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class Security
+    {
+        public bool vpn { get; set; }
+        public bool proxy { get; set; }
+        public bool tor { get; set; }
+        public string isp { get; set; }
+    }
+}
